3b290f91876f3917e636ba9753797a77da818103
canicjusz <55652950+canicjusz@users.noreply.github.com>
2020-12-30 11:38:08 +0100

    Add files via upload

---
+++ kaios-native-ui.js
@@ -0,0 +1,15 @@
+let classesWithColoredParents = /checkbox-container__checkbox|radio-container__radio|input-container__input|textarea-container__textarea|slider-container__slider/g;
+
+const callFunction = (callback, e) => {
+  let element = e.target;
+  //if element has any of those classes in regex then its parent will change class. 
+  if (element.className && element.className.match(classesWithColoredParents))
+    callback(element.parentElement);
+};
+
+const blur = (element) => element.classList.remove('selected');
+
+const focus = (element) => element.classList.add('selected');
+
+window.addEventListener('focus', e => callFunction(focus, e), true);
+window.addEventListener('blur', e => callFunction(blur, e), true);
